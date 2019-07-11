f8ea102569f5b49d588490fefb76ac70105f2526
serg <yelagins@gmail.com>
2019-07-11 20:49:27 +0300

    icons nav footer

--- src/components/RemmeProducers/index.js
+++ src/components/RemmeProducers/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
 import QueueAnim from 'rc-queue-anim';
 import { Tag } from 'antd';
 import numeral from 'numeral';
