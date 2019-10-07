47fdabd9bb9e6a6b444b914b425c9a80e8704f11
serg <yelagins@gmail.com>
2019-10-07 15:22:48 +0300

    css fix

--- server/daemons/rewards.deamon.js
+++ server/daemons/rewards.deamon.js
@@ -31,16 +31,21 @@ const getTotalRewards = async () => {
         pos = pos - 1000;
       }
 
+
+
       difference = DifferenceInDays(date1, actions.slice(-1)[0].block_time);
+
+      console.log(difference);
 
       actions.forEach(i => {
         if (i.action_trace.act.name == "torewards" && DifferenceInDays(date1, i.block_time) < 4) {
+          //console.log(i.block_time);
           sum = sum + Number(i.action_trace.act.data.amount.split(' ')[0])
         }
       })
     } while (pos > 0 && difference < 4);
     console.log('\x1b[32m%s\x1b[0m', '[REWARDS DEAMON] Done:', sum);
-    return sum / difference
+    return sum / 4
 }
 
 export const startRewardsDeamon = async () => {
