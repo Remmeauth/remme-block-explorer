b365dc2212a779b637c21449108da86c7e97a532
serg <yelagins@gmail.com>
2019-12-20 16:42:05 +0200

    build main

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.285113dce24c3309a5359e0b82219976.js"
+  "/precache-manifest.0eea40b558e4263a11c4edf794e4fe8e.js"
 );
 
 self.addEventListener('message', (event) => {
