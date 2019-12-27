851bd3af5204d3be7b3cc073290631354b45f7d0
serg <yelagins@gmail.com>
2019-12-27 17:10:55 +0200

    config changes

--- config.example.js
+++
@@ -1,6 +0,0 @@
-export const dateFormat = "YYYY-MM-DD HH:mm";
-export const marketChartEndpoint = 'https://api.coingecko.com/api/v3/coins/remme/market_chart?vs_currency=usd&days=1';
-
-export const coin = 'REM';
-export const nodeAddress = 'http://localhost:8000';
-export const backendAddress = 'http://localhost:3003';
